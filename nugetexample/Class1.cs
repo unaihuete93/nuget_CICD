b48732d7e62259e6e42fc1d75835d184c862fc24
Unai Huete Beloki <unhueteb@microsoft.com>
2019-10-15 20:13:00 +0200

    a

---
+++ nugetexample/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nugetexample
+{
+    public class Unai
+    {
+        int a = 1;//nshfjskfdhdsf
+    }
+}
