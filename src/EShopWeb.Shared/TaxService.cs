298f2a931bb36b07b01a06a3df2da0c44f60a97c
Unai Huete Beloki <64772417+unaihuete93@users.noreply.github.com>
2025-09-29 15:22:37 +0200

    +semver: minor
    Added a test comment to the TaxService class.

--- src/EShopWeb.Shared/TaxService.cs
+++ src/EShopWeb.Shared/TaxService.cs
@@ -1,6 +1,7 @@
 ﻿namespace EShopWeb.Shared;
 public class TaxService
 {
+    //test comment
     static public decimal CalculateTax(decimal taxable, string postalCode)
     {
         return taxable * (decimal).1;
